ee6deba933ddfa619f571365fb1dffb1329852ec
Aaron Freytag <aaroncfreytag@gmail.com>
2019-09-30 16:02:36 -0700

    Reworked the new generator to not use a lib (so it can be used scripting side)

--- src/core/id-generator.ts
+++ src/core/id-generator.ts
@@ -1,18 +1,31 @@
-import { ObjectID } from "mongodb";
+// This ID is manually generated based on the MongoDB specifications
+// https://docs.mongodb.com/manual/reference/bson-types/#objectid
+// This is so this can be used scripting-side
 
 export default class IDGenerator {
-    public static generate(): string {
-        // Start with https://docs.mongodb.com/manual/reference/bson-types/#objectid
-        // const hexString = ObjectID.createFromTime(time).toHexString();
-        const hexString = new ObjectID().toHexString();
-        return this.makeFromHexString(hexString);
+    private counter: number;
+    private counterMax = 16777216;
+    private randomMax = 1099511627776;
+    constructor(counterSeed: number) {
+        this.counter = Math.floor(this.counterMax * counterSeed);
     }
-    public static makeFromHexString(hexString: string) {
+    public makeFrom(time: number, seed: number): string {
+        const id =
+            Math.floor(seed * this.randomMax).toString(16).padStart(10, "0")
+            + this.counter.toString(16).padStart(6, "0")
+            + Math.floor(time / 1000).toString(16).padStart(8, "0");
+        this.counter = this.nextCounterValue(this.counter);
+        return id;
+    }
+    public makeFromHexString(hexString: string) {
         // Rearrange it so it's more obvious that IDs are different at a glance
         // In other words, put the timestamp part at the end, leaving the random value and counter before it
         return hexString.substr(8, 16) + hexString.substr(0, 8);
     }
-    public static makeHexStringFromID(id: string) {
+    public makeHexStringFromID(id: string) {
         return id.substr(16, 8) + id.substr(0, 16);
+    }
+    private nextCounterValue(current: number) {
+        return this.counter + 1 >= this.counterMax ? 0 : current + 1;
     }
 }
--- tests/core/id-generator.unit.test.ts
+++ tests/core/id-generator.unit.test.ts
@@ -1,18 +1,28 @@
 import IDGenerator from "core/id-generator";
 
+let idGenerator!: IDGenerator;
+
+beforeEach(() => {
+    idGenerator = new IDGenerator(0.5);
+});
+
 describe("IDGenerator", () => {
+    test("can generate an ID from time and a seed", () => {
+        const id = idGenerator.makeFrom(1569883975525, 0.2);
+        expect(id).toEqual("33333333338000005d928747");
+    });
     test("can convert mongo ID to game ID", () => {
-        const id = IDGenerator.makeFromHexString("0123456789ABCDEF00112233");
+        const id = idGenerator.makeFromHexString("0123456789ABCDEF00112233");
         expect(id).toEqual("89ABCDEF0011223301234567");
     });
     test("can convert game ID to mongo ID", () => {
-        const id = IDGenerator.makeHexStringFromID("89ABCDEF0011223301234567");
+        const id = idGenerator.makeHexStringFromID("89ABCDEF0011223301234567");
         expect(id).toEqual("0123456789ABCDEF00112233");
     });
     test("can generate many unique IDs at once", () => {
         const ids: {[id: string]: boolean} = {};
         for (let i = 0; i < 100000; i++) {
-            const id = IDGenerator.generate();
+            const id = idGenerator.makeFrom(1569883975525, Math.random());
             if (ids[id]) {
                 throw new Error("Generated ID was not unique");
             }
