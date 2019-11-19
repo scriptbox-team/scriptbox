d91c60525c9dc244e351d0c69e30fdaac9051d0d
Aaron Freytag <aaroncfreytag@gmail.com>
2019-11-19 01:17:51 -0800

    Fixed a very small thing

--- src/core/systems/display-system.ts
+++ src/core/systems/display-system.ts
@@ -35,7 +35,7 @@ export default class DisplaySystem extends System {
             sprites: {},
             inspectedEntityInfo: {},
             messages: [],
-            sounds: [],
+            sounds: {},
             players: {}
         };
     }
